9e05f3a6ab5e8649b6b4bc646f8d7ae87e518ca1
Michaelninder <fabianeternis@gmail.com>
2026-01-26 21:34:47 +0100

    Initial Commit

---
+++ config.php
@@ -0,0 +1,50 @@
+<?php
+
+return [
+    'services' => [
+        [
+            'address' => 'https://mtex.dev',
+            'name' => 'Landingpage',
+            'description' => 'Our main landingpage',
+            'github' => 'https://github.com/MTEX-dev/static-landingpage',
+        ],
+        [
+            'address' => 'https://tw.mtex.dev',
+            'name' => 'Tailwind Components Libary',
+            'description' => 'Our TailwindCSS component library for rapid UI development.',
+            'github' => 'https://github.com/MTEX-dev/tw.mtex.dev',
+        ],
+        [
+            'address' => 'https://nx.mtex.dev',
+            'name' => 'MTEX Nexus',
+            'description' => 'A lightweight JSON API gateway for seamless data exchange and rapid prototyping.',
+            'github' => 'https://github.com/MTEX-dev/nx.mtex.dev',
+        ],
+        [
+            'address' => 'https://gimy.site',
+            'name' => 'GimySite',
+            'description' => 'Free static website hosting for modern developers.',
+            'github' => 'https://github.com/MTEX-dev/gimy.site',
+        ],
+        [
+            'address' => 'https://getmy.name',
+            'name' => 'GetMyName',
+            'description' => 'A headless API to power your personal portfolio data.',
+            'github' => 'https://github.com/MTEX-dev/getmy.name',
+            'is_deployed' => False,
+        ],
+        [
+            'address' => 'https://getmy.blog',
+            'name' => 'GetMyBlog',
+            'description' => 'Lightweight blogging-api for the minimalist writer.',
+            'github' => 'https://github.com/MTEX-dev/getmy.blog',
+            'is_deployed' => False,
+        ],
+    ],
+
+    'states' => [
+        0 => '',
+        1 => '',
+        2 => '',
+    ]
+];
